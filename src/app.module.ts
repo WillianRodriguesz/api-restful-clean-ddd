c5cfae9d776ccc737a03207f7c498de3de702efd
Willian Rodrigues <willianrdriguesz@gmail.com>
2025-01-08 11:21:48 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EnvConfigService } from './shared/infrastructure/env-config/env-config.service';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from './users/infrastructure/users.module';
+
+@Module({
+  imports: [EnvConfigModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService, EnvConfigService],
+})
+export class AppModule {}
---
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -0,0 +1,26 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'node:path';
+
+@Module({
+  providers: [EnvConfigService],
+  exports: [EnvConfigService],
+})
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          envFilePath: [
+            join(__dirname, `../../../../../.env.${process.env.NODE_ENV}`),
+          ],
+        }),
+      ],
+      providers: [EnvConfigService],
+      exports: [EnvConfigService],
+    };
+  }
+}
